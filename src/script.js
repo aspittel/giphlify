db5cf3ce90f8808a81c43fb7b37353b2df6841b1
Ali Spittel <ali07cat07@gmail.com>
2020-09-21 16:38:41 -0500

    add list view

--- src/script.js
+++ src/script.js
@@ -2,31 +2,48 @@ import Amplify, { API, graphqlOperation } from '@aws-amplify/api'
 import awsConfig from './aws-exports'
 
 import { createGif } from './graphql/mutations'
+import { listGifs } from './graphql/queries'
 
 Amplify.configure(awsConfig)
 
 const createNewGif = async e => {
-  e.preventDefault() // don't refresh the page after form submit
+  e.preventDefault()
 
   const gif = {
-    // grab the value of the `altText` field
     altText: document.getElementById('altText').value,
-    // grab the value of the `url` field
     url: document.getElementById('url').value
   }
 
   try {
-    // Make the API request: provide the createGif operation, provide the user's gif data
     const newGif = await API.graphql(graphqlOperation(createGif, { input: gif }))
-    // Print the data to the console once it comes back
-    console.log(newGif)
-    // Reset the form (make the fields blank again)
+    getGifs()
     document.getElementById('create-form').reset()
   } catch (err) {
-    // If the request fails, print the error message to the console
     console.error(err)
   }
 }
 
-// run our createNewGif function when the form is submitted
+const getGifs = async () => {
+  // select the container element
+  const container = document.querySelector('.container')
+  // reset its current contents
+  container.innerHTML = ''
+  // make a request to get all our gifs
+  const gifs = await API.graphql(graphqlOperation(listGifs))
+  // loop through our gifs and 
+  gifs.data.listGifs.items.map(gif => {
+    // create a new image element
+    const img = document.createElement('img')
+    // add the src attribute to the img
+    img.setAttribute('src', gif.url)
+    // add the alt attribute to the img
+    img.setAttribute('alt', gif.altText)
+    // add the image to the container
+    document.querySelector('.container').appendChild(img)
+  })
+}
+
+// run this function on page load
+getGifs()
+
 document.getElementById('create-form').addEventListener('submit', createNewGif)
